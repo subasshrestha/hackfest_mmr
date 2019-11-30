1403ffb1caa0a1bd4bba1172c1e3a6a1d363711f
subasshrestha <subasshrestha513@gmail.com>
2019-11-30 15:52:12 +0545

    permission give added

--- resources/views/report/show.blade.php
+++ resources/views/report/show.blade.php
@@ -11,6 +11,14 @@
      <br>
             <a href='{{ route('report.edit',$report->id) }}' >Edit</a>
     </div>
+
+    <br>
+    <form method="POST" action='{{ route('givepermission.store')}}'>
+        @csrf
+        <input type="text" name="email" id="email"><br>
+        <input type="hidden" name="id" id="id" value="{{$report->id}}"><br>
+        <input type="submit" value="Give Permission">
+    </form>
           
           
         
