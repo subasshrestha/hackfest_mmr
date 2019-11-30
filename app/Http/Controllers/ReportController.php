3657c1fc5205552a740291b370d772360e063508
Shraddha Bhuju <shraddha.bhuju@gmail.com>
2019-11-30 14:23:25 +0545

    editreport

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Report;
 
 class ReportController extends Controller
 {
--- resources/views/report/create.blade.php
+++ resources/views/report/create.blade.php
@@ -15,14 +15,15 @@
 
                         <div class="contact-clean">
                         <form method="POST" action='{{ route('report.store') }}'>
+                            @csrf
                                     <h2 class="text-center">Report</h2>
-                                    <div class="form-group border-dark"><input class="form-control" type="text" name="name" placeholder="Name"></div>
+                                    <div class="form-group border-dark"><input class="form-control" type="text" name="title" placeholder="Name"></div>
                                     <div class="form-group"><textarea class="form-control" type="text" name="description" placeholder="Description"></textarea></div>
-                                    <div class="form-group"><div class="custom-file">
-                          <input class="form control" type="file" id="customFile">
+                                    <div class="form-group"><div class="custom-file" >
+                          <input class="form control" type="file" id="customFile" multiple=true name='images[]'>
                                 </div>
                         </div>
-                                    <div class="form-group"><button class="btn btn-primary" type="submit">send </button></div>
+                                    <div class="form-group"><button class="btn btn-primary" type="submit">Submit</button></div>
                                 </form>
                             </div>
                             <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
