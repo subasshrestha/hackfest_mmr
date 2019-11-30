46ffa015a88979f8a47ae492b5678f720577e20f
Yunip Shrestha <yunipshrestha@gmail.com>
2019-11-30 11:39:13 +0545

    models

--- app/User.php
+++ app/User.php
@@ -36,4 +36,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function reports()
+    {
+        return $this->hasMany('App\Report');
+    }
+    public function hospital()
+    {
+        return $this->hasOne('App\Hospital');
+    }
+
 }
