4755038911e2e8eee755541a7ef9123a83819383
subasshrestha <subasshrestha513@gmail.com>
2019-11-30 14:34:38 +0545

    reports sent to hospital index

--- app/Http/Controllers/HospitalController.php
+++ app/Http/Controllers/HospitalController.php
@@ -17,9 +17,8 @@ class HospitalController extends Controller
         if(!auth()->user()->hospital()->exists()){
             return view('hospital.create');
         }
-        $users=auth()->user()->hospital->reports;
-        dd($users);
-        return view('hospital.index');
+        $reports=auth()->user()->hospital->reports;
+        return view('hospital.index')->with('reports',$reports);
     }
 
     /**
