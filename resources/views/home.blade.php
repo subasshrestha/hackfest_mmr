9f29d1962845e24ee056fc4ad0b5b090ac405e6a
Shraddha Bhuju <shraddha.bhuju@gmail.com>
2019-11-30 16:03:01 +0545

    show frontedn done

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,6 +8,8 @@
             </div><table id="example" class="table table-striped table-bordered" cellspacing="0" width="100%">
 
                 <h4><a href='{{ route('report.create') }}'>Add Reports</a></h4>
+                <br>
+                
                 <thead>
                     <tr>
                         <th>SN</th>
--- resources/views/report/show.blade.php
+++ resources/views/report/show.blade.php
@@ -1,18 +1,51 @@
 
 @extends('layouts.app')
-
 @section('content')
-    <div class="container">
-            {{$report->title}}<br>
-            {{$report->description}}<br>
-    @foreach($report->images as $image)
-     <img src='/{{$image}}' width='500px'/> 
-     @endforeach
-     <br>
+<div class="table-responsive" style="width: 627px;margin-top: 11px;margin-bottom: 0px;margin-left: 68px;">
+<table class="table">
+                <thead>
+                    <tr>
+                        <th style="background-color: #8dbfd5;">&nbsp;User Details</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>&nbsp;Name:</td>
+                    </tr>
+                    <tr>
+                        <td>&nbsp;Email:</td>
+                    </tr>
+                    <tr>
+                        <td>&nbsp;</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div></div>
+        <div class="table-responsive" style="width: 627px;margin-top: 11px;margin-bottom: 0px;margin-left: 68px;">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th style="background-color: #8dbfd5;">Report Details</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>{{$report->title}}</td>
+                    </tr>
+                    <tr>
+                        <td style="height: 102px;">{{$report->description}}</td>
+                    </tr>
+                    <tr>@foreach($report->images as $image)
+                                <img src='/{{$image}}' width='500px'/> 
+                                @endforeach</tr>
+                </tbody>
+            </table>
             <a href='{{ route('report.edit',$report->id)}}' >Edit</a>
-    </div>
+
+
+    
           
-          
-        
+        </div>  
 
 @endsection
